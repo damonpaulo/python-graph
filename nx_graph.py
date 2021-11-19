94abbcfed2a1f8d0250088856678eb9aa7034db2
dmpaulo <81866815+dmpaulo@users.noreply.github.com>
2021-11-19 17:14:57 -0500

    Complete functionality to allow asset to update status based on components' status

--- nx_graph.py
+++ nx_graph.py
@@ -8,23 +8,35 @@ import collections
 import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
-
+from statistics import mean
 
 class Asset():
-	def __init__(self, attributes, status, status_f):
+	def __init__(self, node_id, attributes, status, status_f):
 		self.status = status
 		self.status_f = status_f
 		self.attributes = attributes
 		self.category = 'asset'
+		self.nid = node_id
+		
+	def __str__(self):
+		return 'Asset ' + str(self.nid)
 			
 	def update_asset(self,*args):
-		self.status = self.status_f(args)
+		if len(args) == 1:
+			comp_stat = [i.status for i in args[0]]
+			self.status = self.status_f(comp_stat)
+		else:
+			self.status = self.status_f(args)
 
 class Component():
-	def __init__(self, attributes, status):
+	def __init__(self, node_id, attributes, status):
 		self.status = status
 		self.attributes = attributes
 		self.category = 'component'
+		self.nid = node_id
+		
+	def __str__(self):
+		return 'Component ' + str(self.nid)
 		
 	def push_updates(self):
 		return
@@ -48,47 +60,56 @@ class Node(Asset, Component):
 def prep_data_for_graph(edge_file,vertex_file,asset_functions=None):
 	E = pd.read_csv(edge_file)
 	V = pd.read_csv(vertex_file).set_index('VERTEX')
-	print(E)
+	print('E:',E)
 	# Make keys for Vertex List - V
 	V['ATTRIBUTES'] = V.apply((lambda row: row['ATTRIBUTES'].split(';')), axis=1)
-	print(V)
+	print('V:',V)
 	Nodes = list(V['CATEGORY'])
 	
 	for i, n in enumerate(Nodes):
-		#print(V.iloc[i,2:])
-		#print(n)
 		if n == 'asset':
-			Nodes[i] = Asset(V.iloc[i,1],V.iloc[i,2],V.iloc[i,3])
+			Nodes[i] = Asset(i,V.iloc[i,1],V.iloc[i,2],asset_functions[V.iloc[i,3]])
 		else:
-			Nodes[i] = Component(V.iloc[i,1],V.iloc[i,2])
+			Nodes[i] = Component(i,V.iloc[i,1],V.iloc[i,2])
 	
 	V['NODE'] = Nodes
-	
-	for idx, row in E.iterrows():
-		print(idx, row)
-		
+
 	E['FROM'] = E.apply((lambda row: V.iloc[row[0]]['NODE']), axis=1)
 	E['TO'] = E.apply((lambda row: V.iloc[row[1]]['NODE']), axis=1)
-	return E
+	return (V, E)
 
 def main():
-	#G = Graph(pd.read_csv('g3.csv'),V=pd.read_csv('g3v.csv'))
-	#G.print_graph()
-	#E = G.E
-	
-	#Gnx = nx.from_pandas_edgelist(E,source='FROM',target='TO',edge_attr='WEIGHT',create_using=nx.DiGraph())
-	
-	#nx.draw(Gnx, with_labels=True)
-	#plt.savefig('graph2.png')
+	#define functions
+	def w_mean(*args):
+		return
+		#weighted mean would need weights passed
 		
-	#V = G.V
-	#print(V[0].value.status)
-	#V[0].value.update_asset(V[1].value.status,V[2].value.status)
-	#print(V[0].value.status)
-	G = nx.from_pandas_edgelist(prep_data_for_graph('edge_in.csv','vertex_in.csv'),source='FROM',target='TO',edge_attr='WEIGHT',create_using=nx.DiGraph())
 	
-	nx.draw(G, with_labels=True)
-	plt.savefig('graph3.png')
+	functions = {'mean': mean, 'weighted mean': w_mean}
+	
+	(V, E) = prep_data_for_graph('edge_in.csv','vertex_in.csv',functions)
+	
+	G = nx.from_pandas_edgelist(E,source='FROM',target='TO',edge_attr='WEIGHT',create_using=nx.DiGraph())
+	
+	node_str = [str(node) for node in G.nodes()]
+	node_type = [type(node) for node in G.nodes()]
+	lbl = dict(zip(G.nodes(),node_str))
+	
+	col = ['palegreen' if n is Asset else 'deepskyblue' for n in node_type]
+	#shp = ['h' if n is Asset else 'o' for n in node_type] #not sure if this is possible #shape options 'so^>v<dph8'
+	nx.draw_circular(G, labels=lbl, with_labels=True,font_size=10,font_color='darkslategray',node_color=col,node_size=6000,node_shape='o',alpha=0.67)
+	plt.savefig('graph.png')
+	
+	assets = list(V[V['NODE'].apply(lambda x: isinstance(x, Asset))]['NODE'])
+	print(assets)
+	
+	for i, n in enumerate(nx.all_neighbors(G, assets[0])):
+		print(i, n)
+		
+	for a in assets:
+		print(a.status)
+		a.update_asset(nx.all_neighbors(G,a))
+		print(a.status)
 	
 	return
 
