2b86816a27d083e69cdb8ca83bcade009240d608
dmpaulo <81866815+dmpaulo@users.noreply.github.com>
2021-11-23 16:50:49 -0500

    Update nx_graph.py to include distance in addition to weight in edge data

--- nx_graph.py
+++ nx_graph.py
@@ -10,108 +10,105 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from statistics import mean
 
-class Asset():
-	def __init__(self, node_id, attributes, status, status_f):
-		self.status = status
-		self.status_f = status_f
-		self.attributes = attributes
-		self.category = 'asset'
+class Node():	
+	def __init__(self, node_id=0, category='node', attributes=[], status=0, function=None):
 		self.nid = node_id
+		self.category = category
+		self.attributes = attributes
+
+		self.category = category
+		self.status = status
+		self.function = function
+		
+	def __repr__(self):
+		return 'Node obj.: '+repr(self.nid)
 		
 	def __str__(self):
-		return 'Asset ' + str(self.nid)
-			
-	def update_asset(self,*args):
+		return self.category.capitalize()+' '+str(self.nid)
+		
+	# pass in-neighbors as args
+	def update_node(self,*args):
 		if len(args) == 1:
 			comp_stat = [i.status for i in args[0]]
-			self.status = self.status_f(comp_stat)
+			self.status = self.function(comp_stat)
 		else:
-			self.status = self.status_f(args)
+			self.status = self.function(args)
 
-class Component():
-	def __init__(self, node_id, attributes, status):
-		self.status = status
-		self.attributes = attributes
-		self.category = 'component'
-		self.nid = node_id
-		
-	def __str__(self):
-		return 'Component ' + str(self.nid)
-		
-	def push_updates(self):
-		return
-		# could implement this to force associated assets to update
-
-class Node(Asset, Component):	
-	def __init__(self, node_id=0, neighbors=set(), children=collections.deque(), 
-	             attributes=[], category='node', function=None):
-		self.neighbors = neighbors
-		self.attributes = attributes
-		self.nid = node_id
-		self.children = children
-		self.category = category
-		if category == 'asset':
-			self.value = Asset(0, function)
-		elif category == 'component':
-			self.value = Component(1)
+	#pass in-edges as args
+	def update_node_weighted(self,*args):
+		if len(args) == 1:
+			stat_weight = [(i[0].status, i[2]['WEIGHT']) for i in args[0]]
+			self.status = self.function(stat_weight)
 		else:
-			self.value = category
+			self.status = self.function(args)
+			
 	
-def prep_data_for_graph(edge_file,vertex_file,asset_functions=None):
+def prep_data_for_graph(edge_file,vertex_file,asset_functions):
 	E = pd.read_csv(edge_file)
 	V = pd.read_csv(vertex_file).set_index('VERTEX')
-	print('E:',E)
-	# Make keys for Vertex List - V
+	print('E:\n',E,'\n')
 	V['ATTRIBUTES'] = V.apply((lambda row: row['ATTRIBUTES'].split(';')), axis=1)
-	print('V:',V)
-	Nodes = list(V['CATEGORY'])
+	V['FUNCTION'] = V['FUNCTION'].fillna(0)
+	Nodes = []
 	
-	for i, n in enumerate(Nodes):
-		if n == 'asset':
-			Nodes[i] = Asset(i,V.iloc[i,1],V.iloc[i,2],asset_functions[V.iloc[i,3]])
-		else:
-			Nodes[i] = Component(i,V.iloc[i,1],V.iloc[i,2])
+	
+	for i in range(len(V)):
+		Nodes.append(Node(i,V.iloc[i,0],V.iloc[i,1],V.iloc[i,2],asset_functions[V.iloc[i,3]]))
 	
 	V['NODE'] = Nodes
+	print('V:\n',V,'\n')
 
 	E['FROM'] = E.apply((lambda row: V.iloc[row[0]]['NODE']), axis=1)
 	E['TO'] = E.apply((lambda row: V.iloc[row[1]]['NODE']), axis=1)
 	return (V, E)
 
 def main():
-	#define functions
+	#define example you want
+	example = 'ex2'
+	
 	def w_mean(*args):
-		return
-		#weighted mean would need weights passed
+		stat_weight = args[0]
+		num, denom = 0, 0
+		for (s, w) in stat_weight:
+			num += s*w
+			denom += w
+			
+		return num/denom
 		
+	#define functions in python based on string input (need 0: None for null function)
+	functions = {0: None, 'mean': mean, 'wmean': w_mean}
 	
-	functions = {'mean': mean, 'weighted mean': w_mean}
+	if example == 'ex2':
+		edge_file, vertex_file = 'edge_in_2.csv', 'vertex_in_2.csv'
+	else:
+		edge_file, vertex_file = 'edge_in.csv', 'vertex_in.csv'
 	
-	(V, E) = prep_data_for_graph('edge_in.csv','vertex_in.csv',functions)
+	(V, E) = prep_data_for_graph(edge_file,vertex_file,functions)
 	
-	G = nx.from_pandas_edgelist(E,source='FROM',target='TO',edge_attr='WEIGHT',create_using=nx.DiGraph())
+	G = nx.from_pandas_edgelist(E,source='FROM',target='TO',edge_attr=['WEIGHT','DIST'],create_using=nx.DiGraph())
+	#print(G.edges.data())
 	
-	node_str = [str(node) for node in G.nodes()]
-	node_type = [type(node) for node in G.nodes()]
-	lbl = dict(zip(G.nodes(),node_str))
-	
-	col = ['palegreen' if n is Asset else 'deepskyblue' for n in node_type]
-	#shp = ['h' if n is Asset else 'o' for n in node_type] #not sure if this is possible #shape options 'so^>v<dph8'
-	nx.draw_circular(G, labels=lbl, with_labels=True,font_size=10,font_color='darkslategray',node_color=col,node_size=6000,node_shape='o',alpha=0.67)
-	plt.savefig('graph.png')
-	
-	assets = list(V[V['NODE'].apply(lambda x: isinstance(x, Asset))]['NODE'])
-	print(assets)
-	
-	for i, n in enumerate(nx.all_neighbors(G, assets[0])):
-		print(i, n)
+	assets = V['NODE'].loc[V['CATEGORY']=='asset']
+	components = V['NODE'].loc[V['CATEGORY']=='component']
 		
 	for a in assets:
-		print(a.status)
-		a.update_asset(nx.all_neighbors(G,a))
-		print(a.status)
+		print('Status of',a,'before update:',a.status)
+		if example == 'ex2':
+			a.update_node_weighted(G.in_edges(a,data=True))
+			for e in G.in_edges(a,data=True):
+				print(e)
+		else:
+			a.update_node(G.predecessors(a))
+		
+		print('Status of',a,'after  update:',a.status,'\n')
+	
+	#draw graph
+	col = ['palegreen' if n.category=='asset' else 'deepskyblue' for n in G.nodes()]
+	nx.draw_spectral(G, with_labels=True,font_size=10,node_color=col,node_size=5000,node_shape='o',alpha=0.7)
+	plt.savefig('graph-'+example+'.png')
+	
 	
 	return
-
+	
 if __name__ == '__main__':
 	sys.exit(main())
